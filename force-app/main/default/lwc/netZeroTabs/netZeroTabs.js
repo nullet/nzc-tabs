31058fe0dfdfb7747b7381aa8a15558d1757f51f
Tom Nullet <tomnullet@gmail.com>
2022-03-23 13:38:48 -0500

    -> . links work.

--- force-app/main/default/lwc/netZeroTabs/netZeroTabs.js
+++ force-app/main/default/lwc/netZeroTabs/netZeroTabs.js
@@ -1,10 +1,32 @@
 import { LightningElement } from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 
-export default class NetZeroTabs extends LightningElement {
+export default class NetZeroTabs extends NavigationMixin(LightningElement) {
 
   handleSetActiveSectionStationary() {
     const accordion = this.template.querySelector('.nzc-tabs');
 
-    accordion.activeSectionName = 'Stationary';
+    accordion.activeSectionName = 'Stationary';  
   }
+
+  navToStatAssetCrbnFtprnt() {
+    this[NavigationMixin.Navigate]({
+      type: 'standard__objectPage',
+      attributes: {
+        objectApiName: 'ns__CrbnEmssnScopeAllocVal',
+        actionName: 'list'
+      }
+    });
+  };
+
+  navToS3EmFctrs() {
+    this[NavigationMixin.Navigate]({
+      type: 'standard__objectPage',
+      attributes: {
+        objectApiName: 'sustain_app__EmissionFactorScope3__c',
+        actionName: 'list'
+      }
+    })
+  };
+
 }
